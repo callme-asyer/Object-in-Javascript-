935bd79a15c8828aa70e09a955f6836ad3e2614d
callme-asyer <53029091+callme-asyer@users.noreply.github.com>
2019-10-04 08:27:18 +0700

    Tugas Niomic
    Tugas 8.js

---
+++ tugas 8.js
@@ -0,0 +1,13 @@
+function belajarObject(){
+  var dataMahasiswa= {
+     nama: 'alfredo asyer limbor',
+     nim :  50160383,
+     jurusan: 'teknik informatika'
+
+  }
+   console.log("data mahasiswa Universitas Kwik Kian Gie : \n" + dataMahasiswa.nama + " \n" + dataMahasiswa.nim + " \n" + dataMahasiswa.jurusan);
+
+
+}
+
+belajarObject();
